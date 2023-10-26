c224f14f47c014df34768f8d72833f159547b6be
kianabel <kian.leba@gmail.com>
2023-10-27 00:17:11 +0200

    yallah

---
+++ bot.js
@@ -0,0 +1,6 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+const token = 'YOUR_BOT_TOKEN';
+
+client.login(token);
